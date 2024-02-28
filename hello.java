6daa38ba67679d473c4ac17d8cff397fdeb3a6e2
nithyashree2 <160814981+nithyashree2@users.noreply.github.com>
2024-02-28 12:19:19 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,8 +3,8 @@ public class hello
 	public static void main(String args[])
 	{
 		System.out.println("welcome to spt");
-		System.out.println("mysore");
-		System.out.println("bangalore spt");
+		System.out.println("mysore spt");
+		
 	}
 }
 
